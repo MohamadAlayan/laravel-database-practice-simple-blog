2a22e5b836175f37c051faa0f8ce04cc9cbf0190
Mohmad Alayan <mohamadalayanlb@gmail.com>
2022-09-11 23:23:57 +0300

    Second Commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->hasPosts(mt_rand(2, 6))->create();
+    }
+}
